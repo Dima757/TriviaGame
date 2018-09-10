fd05df8ea8bbae50b64a51f55299c0f8c5925bbc
Dima <dima757@hotmail.com>
2018-09-10 17:14:22 -0500

    anothaone

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+// Global Variables
+// =========================================
+var trivia = {
+    initialScreen: "",
+    correctCounter: 0,
+    inCorrectCounter: 0,
+    unAnsweredCounter: 0,
+    clickSound: new Audio(),
+    gameHTML: "",
+    questionsArray: [],
+    answerArray: [],
+    correctAnswers: [],
+    imageArray: [],
+    clock: "",
+    questionCounter: 0,
+    timeCounter: 20,
+
+};
